a11514470792c9ca52de377e0ea650d9231839db
桑晓龙 <sangxl@iict.ac.cn>
2022-03-01 17:35:19 +0800

    关闭graphql

--- stop.sh
+++ stop.sh
@@ -10,6 +10,6 @@ echo "*************************************${normal}"
 echo "Stop docker containers"
 echo "-------------------------------------"
 
-# export BOOTNODE=$(cat nodes/validator1/key.pub)
+export BOOTNODE=$(cat nodes/validator1/key.pub)
 
 docker-compose -f dockers/$1.yml down -v
